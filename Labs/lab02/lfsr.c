200c69540d470883b1e127632a98af9105a5294e
rougeLi <rouge_3877@outlook.com>
2024-01-24 21:16:28 +0800

    finish lfsr

--- Labs/lab02/lfsr.c
+++ Labs/lab02/lfsr.c
@@ -6,5 +6,18 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    /*get bit*/
+    uint16_t _0th_bit_ = ((*reg) & (1 << 0)) >> 0;
+    uint16_t _2th_bit_ = ((*reg) & (1 << 2)) >> 2;
+    uint16_t _3th_bit_ = ((*reg) & (1 << 3)) >> 3;
+    uint16_t _5th_bit_ = ((*reg) & (1 << 5)) >> 5;
+    /*calculate bit*/
+    uint16_t _15th_bit_ = ((_0th_bit_ ^ _2th_bit_) ^ _3th_bit_) ^ _5th_bit_;
+    /*write current bit and shift   */   
+    uint16_t _writen_15th_bit_ = _15th_bit_ << 15;
+    uint16_t _15th_bit_eq0_ = 0b0111111111111111;
+    (*reg) = (*reg) >> 1;
+    (*reg) = ((*reg) & _15th_bit_eq0_) | _writen_15th_bit_;
+    return;
 }
 
