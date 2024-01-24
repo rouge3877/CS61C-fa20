16a09328494c43d0924650c20b960263707e7aa1
rougeLi <rouge_3877@outlook.com>
2024-01-24 18:19:02 +0800

    finish exercise1-bit_ops

--- Labs/lab02/bit_ops.c
+++ Labs/lab02/bit_ops.c
@@ -9,6 +9,13 @@ unsigned get_bit(unsigned x,
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns
     // 0 or 1)
+    if(n <=31 && n >= 0){
+        unsigned temp = 1 << n;
+        temp = temp & x;
+        temp >>= n;
+        return temp;
+    }else
+
     return -1;
 }
 // Set the nth bit of the value of x to v.
@@ -17,11 +24,18 @@ void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
     // YOUR CODE HERE
+    unsigned temp = v << n;
+    unsigned nth_bit = ~(1 << n);
+    (*x) = (*x) & nth_bit ;
+    (*x) = (*x) | temp;    
+    return;
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
     // YOUR CODE HERE
+    unsigned temp = 1 << n;
+    (*x) = (*x) ^ temp;
 }
 
